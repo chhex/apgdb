43ec0c0b087d36ec9f6135e61fc3c9c27769864e
Christoph Henrici <chhenrici@gmail.com>
2020-09-01 10:50:21 +0200

    Initial

--- test/test_sequel.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env ruby
-# encoding: utf-8
-require_relative "../lib/apgdb"
-seq = Sequel::Connection.new("CHEI212")
-DB =  seq.connect('che','xxxxxxx')
-DB['select * from DRUCKER_f'].each do |row|
-  p row
-end
